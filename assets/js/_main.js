29c0ebc635b60cedd9b13929e9fd4ebcc26fcd1c
Micah Rairdon <micah.rairdon@Haworth.com>
2020-11-21 21:11:04 -0500

    Social-share fixes

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -90,7 +90,7 @@ $(document).ready(function(){
 //sharing
 $(document).ready(function(){
   $("body").floatingSocialShare({
-    buttons: ["facebook","twitter","google-plus", "linkedin", "pinterest"],
+    buttons: ["facebook","twitter","linkedin", "pinterest"],
     text: "Share with "
   });
 });
